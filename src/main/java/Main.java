653509d538ab02451e10b7d6e13df4628a41dc6f
Anestis Vlasopoulos <anastasios.vlasopoulos@gmail.com>
2019-10-13 10:08:16 +0300

    id: 1 description: Build-run jar

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,9 +4,10 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Program starts...");
-        System.out.println("Please define path of user files");
+        System.out.println("Please define path of user files:");
         BufferedReader reader =
                 new BufferedReader(new InputStreamReader(System.in));
+
 
     }
 }
