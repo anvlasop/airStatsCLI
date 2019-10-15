d361f32a5e28cd547128c0198dcaf8c89e99cf11
Anestis Vlasopoulos <anastasios.vlasopoulos@gmail.com>
2019-10-15 23:56:39 +0300

    id: 12 description: Calculate airline amount per user (unfinished). Sonar issue resolving

--- src/main/java/core/isolatotion/UserIsolatorProcess.java
+++ src/main/java/core/isolatotion/UserIsolatorProcess.java
@@ -8,25 +8,35 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.text.ParseException;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 public class UserIsolatorProcess {
+
+    private HashMap<Long, Purchases> purchasesPerUser;
+
     public void startIsolation() {
         PathReader pathReader = new PathReader();
         File path = pathReader.readPath();
         boolean isDirectory = pathReader.checkIfIsDirectory(path);
         if (!isDirectory) {
-            System.out.printf("The given path does not belong to a directory. \n" +
+            System.out.println("The given path does not belong to a directory. \n" +
                     "Run this program again and define the right path");
         } else {
             extractUsersDataFromFile(path);
+            purchasesPerUser.forEach((aLong, purchases) -> {
+                purchases.getPurchases().stream();
+            });
+
+            System.out.println("sdf");
+
         }
     }
 
-    private HashMap<Long, Purchases> extractUsersDataFromFile(File path) {
+    private void extractUsersDataFromFile(File path) {
 
-        HashMap<Long, Purchases> purchasesPerUser = new HashMap<>();
+        purchasesPerUser = new HashMap<>();
         File[] files = path.listFiles();
         List<File> fileList = Arrays.asList(files);
         fileList.forEach(file -> {
@@ -34,8 +44,7 @@ public class UserIsolatorProcess {
                 String fileName = file.getName();
                 Long userId = Long.parseLong(fileName.split("\\.")[0]);
                 StringBuilder stringBuilder = new StringBuilder();
-                Files.lines( Paths.get(file.getPath()), StandardCharsets.UTF_8).forEach(s ->
-                        stringBuilder.append(s));
+                Files.lines( Paths.get(file.getPath()), StandardCharsets.UTF_8).forEach(s -> stringBuilder.append(s));
 
                 Purchases purchases = extractPurchases(stringBuilder);
                 purchasesPerUser.put(userId, purchases);
@@ -45,16 +54,12 @@ public class UserIsolatorProcess {
                 System.out.println("Error occurred during reading file.");
             }
         });
-
-        return purchasesPerUser;
-
     }
 
     private Purchases extractPurchases(StringBuilder stringBuilder) throws com.fasterxml.jackson.core.JsonProcessingException {
         String userPurchasesString = stringBuilder.toString();
         ObjectMapper mapper = new ObjectMapper();
-        Purchases purchases = mapper.readValue(userPurchasesString, Purchases.class);
-        return purchases;
+        return mapper.readValue(userPurchasesString, Purchases.class);
     }
 
 
