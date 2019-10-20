5aa1e92bcb18b9b5e531451fc61b0547e6078955
Anestis Vlasopoulos <anastasios.vlasopoulos@gmail.com>
2019-10-20 23:15:36 +0300

    id: 19 description: Calculate/report anonymize data and statistical results

--- src/main/java/core/anonymization/AnonymizationProcess.java
+++ src/main/java/core/anonymization/AnonymizationProcess.java
@@ -2,10 +2,7 @@ package core.anonymization;
 
 import core.isolatotion.Purchase;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class AnonymizationProcess {
@@ -20,6 +17,53 @@ public class AnonymizationProcess {
     }
 
     private void calculateAmountCount() {
-        Map<Object, Long> amountCountMap = allAirlineAmounts.stream().collect(Collectors.groupingBy(amount -> amount, Collectors.counting()));
+        Map<Float, Long> amountCountMap = allAirlineAmounts.stream().collect(Collectors.groupingBy(amount -> amount, Collectors.counting()));
+        amountCountMap.values().removeIf(countOfAmounts -> countOfAmounts <= 5);
+        reportAnonymizedCalculations(amountCountMap);
+        extractStatisticalResults(amountCountMap.keySet());
     }
+
+    private void reportStatisticalResults(Float minAmount, Float maxAmount, Double averageAmount, Float medianAmount) {
+        System.out.println("min: " + minAmount);
+        System.out.println("max: " + maxAmount);
+        System.out.println("average: " + averageAmount);
+        System.out.println("median:" + medianAmount);
+    }
+
+    private Double extractAverage(Set<Float> amounts) {
+        OptionalDouble averageAmountOptional = amounts.stream().mapToDouble(amount -> amount).average();
+        return averageAmountOptional.isPresent() ? averageAmountOptional.getAsDouble() : 0;
+    }
+
+    private Float calculateMedianAmount(Set<Float> amounts) {
+        List<Float> amountList = amounts.stream().collect(Collectors.toList());
+        double halfSize = amountList.size() / 2;
+        Collections.sort(amountList);
+        if (amountList.size() % 2 != 0) {
+            int indexOfMedian = (int) Math.floor(halfSize);
+            return (amountList.get(indexOfMedian));
+         } else {
+            int leftIndex = (int) Math.floor(halfSize);
+            int rightIndex = (int) Math.ceil(halfSize);
+            return ( amountList.get(leftIndex) + amountList.get(rightIndex) ) / 2;
+
+         }
+
+    }
+
+    private void extractStatisticalResults(Set<Float> amounts) {
+        Float minAmount = Collections.min(amounts);
+        Float maxAmount = Collections.max(amounts);
+        Double averageAmount = extractAverage(amounts);
+        Float medianAmount = calculateMedianAmount(amounts);
+
+        reportStatisticalResults(minAmount, maxAmount, averageAmount, medianAmount);
+
+    }
+
+    private void reportAnonymizedCalculations(Map<Float,Long> amountCountMap) {
+        amountCountMap.forEach((amount, count) -> System.out.println("| Airline purchase | " + amount + " | " + count + " |"));
+    }
+
+
 }
